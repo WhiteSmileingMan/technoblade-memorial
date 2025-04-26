f86de335b02c77a1d61f0644a21a1e14a2c9a73d
WhiteSmileingMan <angyuray32@gmail.com>
2025-04-26 15:25:46 +0700

    1.21.2 Support

--- src/main/java/org/example/technomemorial/feature/CrownRenderer.java
+++ src/main/java/org/example/technomemorial/feature/CrownRenderer.java
@@ -7,27 +7,29 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
 import net.minecraft.client.render.entity.model.EntityModel;
-import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
 
-public class CrownRenderer<S extends LivingEntityRenderState, M extends EntityModel<? super S>> extends FeatureRenderer<S, M> {
+public class CrownRenderer<T extends Entity, M extends EntityModel<T>> extends FeatureRenderer<T, M> {
 
-    private final Identifier TEXTURE = Identifier.ofVanilla("textures/entity/pig/crown.png");
+    private final Identifier TEXTURE = Identifier.of("technomemorial","textures/entity/pig/crown.png");
     private final M model;
-    private final M babyModel;
 
-    public CrownRenderer(FeatureRendererContext<S, M> context, M model, M babyModel) {
+    public CrownRenderer(FeatureRendererContext<T, M> context, M model) {
         super(context);
         this.model = model;
-        this.babyModel = babyModel;
     }
-    public void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, S livingEntityRenderState, float f, float g) {
-        if (livingEntityRenderState.customName != null && livingEntityRenderState.customName.getString().equals("Technoblade")) {
-            M entityModel = livingEntityRenderState.baby ? this.babyModel : this.model;
-            entityModel.setAngles(livingEntityRenderState);
-            VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(RenderLayer.getEntityCutoutNoCull(this.TEXTURE));
-            entityModel.render(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV);
+
+    @SuppressWarnings("DataFlowIssue")
+    @Override
+    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, T entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
+        if (entity.hasCustomName() && entity.getCustomName().getString().equals("Technoblade")) {
+            this.getContextModel().copyStateTo(this.model);
+            this.model.animateModel(entity, limbAngle, limbDistance, tickDelta);
+            this.model.setAngles(entity, limbAngle, limbDistance, animationProgress, headYaw, headPitch);
+            VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getEntityCutoutNoCull(this.TEXTURE));
+            this.model.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV);
         }
     }
 }
