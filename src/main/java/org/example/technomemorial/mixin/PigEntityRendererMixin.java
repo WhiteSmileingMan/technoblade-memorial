8b0b6bbeccb25a53b3c321d64dcd8d5e0cbe6cdc
WhiteSmileingMan <angyuray32@gmail.com>
2025-04-26 15:06:38 +0700

    1.21 Support

--- src/main/java/org/example/technomemorial/mixin/PigEntityRendererMixin.java
+++ src/main/java/org/example/technomemorial/mixin/PigEntityRendererMixin.java
@@ -4,30 +4,33 @@ import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PigEntityRenderer;
 import net.minecraft.client.render.entity.model.EntityModel;
-import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.entity.model.PigEntityModel;
-import net.minecraft.client.render.entity.state.LivingEntityRenderState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@SuppressWarnings({"unchecked", "rawtypes"})
 @Mixin(PigEntityRenderer.class)
-public abstract class PigEntityRendererMixin extends LivingEntityRenderer {
-    public PigEntityRendererMixin(EntityRendererFactory.Context ctx, EntityModel model, float shadowRadius) {
-        super(ctx, model, shadowRadius);
+public abstract class PigEntityRendererMixin extends LivingEntityRenderer<PigEntity, EntityModel<PigEntity>> {
+
+    private final Identifier TEXTURE = Identifier.of("technomemorial","textures/entity/pig/technoblade.png");
+
+    public PigEntityRendererMixin(EntityRendererFactory.Context context, EntityModel<PigEntity> model, float shadowRadius) {
+        super(context, model, shadowRadius);
     }
-    private static final Identifier TEXTURE = Identifier.ofVanilla("textures/entity/pig/technoblade.png");
 
     @Inject(
-            method = "getTexture(Lnet/minecraft/client/render/entity/state/LivingEntityRenderState;)Lnet/minecraft/util/Identifier;",
-            at = @At("HEAD"), cancellable = true
+            method = "getTexture(Lnet/minecraft/entity/Entity;)Lnet/minecraft/util/Identifier;",
+            at = @At("HEAD"),
+            cancellable = true
     )
-    private void overridePigTexture(LivingEntityRenderState state, CallbackInfoReturnable<Identifier> cir) {
-        if (state.customName != null && "Technoblade".equals(state.customName.getString())) {
+    private void overridePigTexture(Entity entity, CallbackInfoReturnable<Identifier> cir) {
+        Text customName = entity.getName();
+        if (customName != null && customName.getString().equals("Technoblade")) {
             cir.setReturnValue(TEXTURE);
         }
     }
